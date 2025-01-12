9009bd97b4cc98a2d152e19cf6ed8c07294cadbc
Bpost129 <Bpost129@yahoo.com>
2025-01-11 23:21:47 -0500

    create java backend with spring boot, connect to mongodb, add dependencies, add movie and review models, add movie controller and service functions, create and test endpoints to show all movies and individual movies

---
+++ src/main/java/dev/bpost/java_sb/JavaSbApplication.java
@@ -0,0 +1,13 @@
+package dev.bpost.java_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bpost/java_sb/JavaSbApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bpost.java_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
