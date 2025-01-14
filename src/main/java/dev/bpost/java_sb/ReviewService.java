cdf30fe08b0158adb24b2d1c56f8ce22695542a1
Bpost129 <Bpost129@yahoo.com>
2025-01-13 23:53:08 -0500

    create review model, repo, controller and service function, test create review functionality

--- src/main/java/dev/bpost/java_sb/ReviewService.java
+++ src/main/java/dev/bpost/java_sb/ReviewService.java
@@ -6,24 +6,20 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
-//import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
-//import static org.apache.logging.log4j.util.Lazy.value;
-//import static org.apache.tomcat.jni.SSLConf.apply;
-
 @Service
 public class ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
     @Autowired
     private MongoTemplate mongoTemplate;
-//    public Review createReview(String reviewBody, String id) {
-//        Review review = reviewRepository.insert(new Review(reviewBody));
-//
-//        mongoTemplate.update(Movie.class)
-//                .matching(Criteria.where(key: "id").is(id))
-//                .apply(new Update().push(key: "reviewIds").value(review))
-//                .first();
-//
-//        return review;
-//    }
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
 }
